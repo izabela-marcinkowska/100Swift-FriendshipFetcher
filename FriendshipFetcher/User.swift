66081d655e98d6d2d9218c00fce5df500b5d6a58
Devy <izabela.marcinkowska@outlook.com>
2024-10-23 10:04:54 +0200

    feat: Create Data structs

---
+++ FriendshipFetcher/User.swift
@@ -0,0 +1,31 @@
+//
+//  User.swift
+//  FriendshipFetcher
+//
+//  Created by Izabela Marcinkowska on 2024-10-23.
+//
+
+import Foundation
+
+struct Response: Codable {
+    var results: [User]
+}
+
+struct User: Codable {
+    var id: UUID
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: String
+    var tags: [String]
+    var friends: [Friend]
+}
+
+struct Friend: Codable {
+    var id: UUID
+    var name: String
+}
