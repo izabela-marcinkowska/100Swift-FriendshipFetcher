1df5f113ee86468166c8bf9f5d2d0677e3250586
Devy <izabela.marcinkowska@outlook.com>
2024-10-23 09:34:23 +0200

    First commit

---
+++ FriendshipFetcher/FriendshipFetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendshipFetcherApp.swift
+//  FriendshipFetcher
+//
+//  Created by Izabela Marcinkowska on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct FriendshipFetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
