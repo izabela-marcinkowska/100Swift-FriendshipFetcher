1aa09c7cde35b27b9cd6461459bd1a3bc59a5516
Devy <izabela.marcinkowska@outlook.com>
2024-10-24 20:45:13 +0200

    feat: Update user struct to model with custom init

--- FriendshipFetcher/User.swift
+++ FriendshipFetcher/User.swift
@@ -6,12 +6,24 @@
 //
 
 import Foundation
+import SwiftData
 
-struct Response: Codable {
-    var results: [User]
-}
 
-struct User: Codable, Identifiable, Hashable {
+@Model
+class User: Identifiable, Hashable, Codable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case isActive
+        case name
+        case age
+        case company
+        case email
+        case address
+        case about
+        case registered
+        case tags
+        case friends
+    }
     var id: UUID
     var isActive: Bool
     var name: String
@@ -20,12 +32,115 @@ struct User: Codable, Identifiable, Hashable {
     var email: String
     var address: String
     var about: String
-    var registered: String
+    var registered: Date
     var tags: [String]
     var friends: [Friend]
+    
+    init(id: UUID, isActive: Bool, name: String, age: Int, company: String, email: String, address: String, about: String, registered: Date, tags: [String], friends: [Friend]) {
+        self.id = id
+        self.isActive = isActive
+        self.name = name
+        self.age = age
+        self.company = company
+        self.email = email
+        self.address = address
+        self.about = about
+        self.registered = registered
+        self.tags = tags
+        self.friends = friends
+    }
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        isActive = try container.decode(Bool.self, forKey: .isActive)
+        name = try container.decode(String.self, forKey: .name)
+        age = try container.decode(Int.self, forKey: .age)
+        company = try container.decode(String.self, forKey: .company)
+        email = try container.decode(String.self, forKey: .email)
+        address = try container.decode(String.self, forKey: .address)
+        about = try container.decode(String.self, forKey: .about)
+        registered = try container.decode(Date.self, forKey: .registered)
+        tags = try container.decode([String].self, forKey: .tags)
+        friends = try container.decode([Friend].self, forKey: .friends)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(isActive, forKey: .isActive)
+        try container.encode(name, forKey: .name)
+        try container.encode(age, forKey: .age)
+        try container.encode(email, forKey: .email)
+        try container.encode(address, forKey: .address)
+        try container.encode(about, forKey: .about)
+        try container.encode(registered, forKey: .registered)
+        try container.encode(tags, forKey: .tags)
+        try container.encode(friends, forKey: .friends)
+        try container.encode(company, forKey: .company)
+    }
+    
+    init() {
+        id = UUID()
+        isActive = false
+        name = ""
+        age = 0
+        company = ""
+        email = ""
+        address = ""
+        about = ""
+        registered = Date()
+        tags = []
+        friends = []
+    }
+    
+    static func == (lhs: User, rhs: User) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
 }
 
-struct Friend: Codable, Identifiable, Hashable {
+@Model
+class Friend: Identifiable, Hashable, Codable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+    }
+    
     var id: UUID
     var name: String
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+    }
+    
+    init(id: UUID, name: String) {
+           self.id = id
+           self.name = name
+       }
+    
+    init() {
+        id = UUID()
+        name = ""
+    }
+    
+    static func == (lhs: Friend, rhs: Friend) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
 }
